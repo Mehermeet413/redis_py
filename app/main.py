cc76d81f79fa9d828848b67f880d757a6e10e435
Meet <mehermeet12@gmail.com>
2025-07-06 18:35:05 +0530

    Multiple ping response

--- app/main.py
+++ app/main.py
@@ -1,23 +1,24 @@
 import socket  # noqa: F401
+import threading
+
+
+def handle_connection(connection):
+    while True:
+        data: bytes = connection.recv(1024)
+        if data == b"":
+            break
+        connection.sendall(b"+PONG\r\n")
+    connection.close()
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    clint_scoket, _= server_socket.accept()
-    try:
-     while True:
-      data =  clint_scoket.recv(1024)  
-      if not data:
-          break
-      clint_scoket.sendall(b"+PONG\r\n")
-    finally:
-     clint_scoket.close()
-
+    while True:
+        connection, _ = server_socket.accept()  # wait for client
+        thread = threading.Thread(target=handle_connection, args=(connection,))
+        thread.start()
 
 if __name__ == "__main__":
     main()
